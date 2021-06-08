a2fc3bb000ddf08a848da9734fb360c76433e982
Lucas Andrius de Oliveira <74381770+LucasAndrius@users.noreply.github.com>
2021-06-08 14:50:28 -0300

    controle contas abertas com static

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -2,6 +2,7 @@ import { Cliente } from "./Cliente.js";
 
 export class ContaCorrente
 {
+    static numeroDeContas = 0;
     agencia;
 
     //privado
@@ -28,6 +29,7 @@ export class ContaCorrente
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        ContaCorrente.numeroDeContas += 1;
     }
 
     //metodos
--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import {ContaCorrente} from './ContaCorrente.js';
 
 
 const cliente1 = new Cliente("Lucas",11122233309);
-
 const cliente2 = new Cliente("Alice",88822233309);
 
 
@@ -11,11 +10,12 @@ const contaCorrenteLucas = new ContaCorrente(1001,cliente1);
 contaCorrenteLucas.depositar(500);
 
 
-const conta2 = new ContaCorrente(102,cliente1);
+const conta2 = new ContaCorrente(102,cliente2);
 
 
 let valor = 200;
 contaCorrenteLucas.transferir(valor, conta2);
 
-console.log(cliente2.cpf);
+
+console.log(ContaCorrente.numeroDeContas);
 
